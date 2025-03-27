e12fdd65a0e0ee253d34468dfe5792ae92f5c6e3
qu1ief <sabirkg@code.edu.az>
2025-03-28 00:39:44 +0400

    Add project files.

---
+++ PustokTask/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PustokTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
