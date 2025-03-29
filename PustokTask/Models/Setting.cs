126742c586476761cacee4959ad74d886731c662
qu1ief <sabirkg@code.edu.az>
2025-03-30 00:29:49 +0400

    setting

--- PustokTask/Models/Setting.cs
+++ PustokTask/Models/Setting.cs
@@ -4,6 +4,7 @@ namespace PustokTask.Models
 {
     public class Setting 
     {
+        public int Id { get; set; } 
         [Key]
         public string Key { get; set; }
         public string Value { get; set; }
