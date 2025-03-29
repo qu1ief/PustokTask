ba7156fe4b275d10f5a6a4279243979efbe8bcaa
qu1ief <sabirkg@code.edu.az>
2025-03-29 13:59:30 +0400

    admin

---
+++ PustokTask/Areas/Manage/Controllers/GenreController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PustokTask.Data;
+
+namespace PustokTask.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+
+    public class GenreController(PustokDbContex pustokDbContex) : Controller
+    {
+        public IActionResult Index()
+        {
+            var genres  = pustokDbContex.Genres.ToList();
+            return View(genres);
+        }
+    }
+}
